a55727c202c83d0a6cc22c89b7d7050ea8423b07
Shabbir <shabbir.ali818@gmail.com>
2024-08-19 00:05:21 +0500

    add user and video models and install aggregates pipelines

---
+++ src/models/Video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const VideoSchema=new mongoose.Schema(
+    {
+      videoFile:{
+        type:String,//cloudinary url
+        required:true
+      },
+      thumbnail:{
+        type:String,//cloudinary url
+        required:true
+      },
+      title:{
+        type:String,
+        required:true,
+
+      },
+      description:{
+        type:String,
+        required:true,
+        
+      },
+      duration:{
+        type:Number,//cloudinary
+        required:true,
+        
+      },
+      views:{
+        type:Number,
+        default:0
+      },
+      isPublished:{
+        type:Boolean,
+        default:true
+      },
+      owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+      }
+    }
+    ,{timestamps:true});
+
+VideoSchema.plugin(mongooseAggregatePaginate)
+export const Video=mongoose.model("Video",UserSchema);
